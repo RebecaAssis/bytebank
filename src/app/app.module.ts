caddf49471bb0febe804a3c733d3eec6a9e22aeb
Rebeca Assis <rebeca.oassis@hotmail.com>
2021-08-09 12:41:02 -0300

    Projeto inicial. Utilizando service e separando responsabilidades.

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { HttpClientModule } from '@angular/common/http';
+
+registerLocaleData(localePt, 'pt');
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent,
+    ExtratoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'},
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,34 @@
+import { TransferenciaService } from './../services/transferencia.service';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService) {
+
+  }
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
